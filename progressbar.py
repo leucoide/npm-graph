cea556782be802a61e54e5805afc4e7dd8eb5025
Yuri <yuri.vasquez91@gmail.com>
2014-11-17 12:09:31 -0200

    sanity safe time display on progressbar

--- progressbar.py
+++ progressbar.py
@@ -6,6 +6,9 @@ import time
 
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
+SECONDS_IN_A_DAY = 86400
+SECONDS_IN_A_HOUR = 3600
+SECONDS_IN_A_MINUTE = 60
 
 
 def terminal_size():
@@ -21,6 +24,8 @@ class ProgressBar(object):
     def __init__(self, taskLength, autoUpdate=True):
         self.taskLength = taskLength
         self.autoUpdate = autoUpdate
+        self.firstTick = True
+        self.createTime = int(time.time())
         self.progress = 0
 
     def update(self):
@@ -53,11 +58,36 @@ class ProgressBar(object):
         if self.autoUpdate:
             self.update()
         nProgress = self.getNormalizedProgress()
+        now = time.time()
+        if not self.firstTick:
+            delay = now - self.lastTime
+            self.meanDelay = (delay + (self.progress-1)*self.meanDelay) / float(self.progress)
+            ticksToFinish = self.taskLength - self.progress
+            timeToFinish = int(self.meanDelay * ticksToFinish)
+            days = timeToFinish / SECONDS_IN_A_DAY
+            surplus = timeToFinish - days * SECONDS_IN_A_DAY
+            hours = (surplus) / SECONDS_IN_A_HOUR
+            surplus = surplus - hours * SECONDS_IN_A_HOUR
+            minutes = (surplus) / SECONDS_IN_A_MINUTE
+            seconds = surplus - minutes * SECONDS_IN_A_MINUTE
+            daysLabel = "" if days <= 0 else "%dd" % days
+            hoursLabel = "00" if hours <= 0 else "%02d" % hours
+            minutesLabel = "00" if minutes <= 0 else "%02d" % minutes
+            secondsLabel = "00" if seconds <= 0 else "%02d" % seconds
+            timeDisplay = "%s %s:%s:%s" % (daysLabel,
+                                           hoursLabel,
+                                           minutesLabel,
+                                           secondsLabel)
 
+        else:
+            # self.meanDelay = now - self.createTime
+            self.meanDelay = 0
+            timeDisplay = ""
         # counter = "%.1f%%" % (nProgress*100)
-        counter = "%d of %d (%.1f%%)" % (self.progress,
-                                         self.taskLength,
-                                         nProgress*100)
+        counter = "%d of %d (%.1f%%) %s" % (self.progress,
+                                            self.taskLength,
+                                            nProgress*100,
+                                            timeDisplay)
         screenLength = terminal_size()[0]
         leftBarLength = int((screenLength-len(counter))/2.0)
         rightBarLength = screenLength - leftBarLength - len(counter)
@@ -66,10 +96,13 @@ class ProgressBar(object):
                                     rightBarLength,
                                     screenLength)
         print CURSOR_UP_ONE + ERASE_LINE
+        self.lastTime = now
         return leftBar + counter + rightBar + "\r"
 
     def tick(self):
         print self,
+        if self.firstTick:
+            self.firstTick = False
 # p = ProgressBar(100000)
 # for i in range(100000):
 #     time.sleep(.001)
